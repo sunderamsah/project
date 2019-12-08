ec195b99088e233485471e78446a61874ecf0f84
sunderamsah <58601200+sunderamsah@users.noreply.github.com>
2019-12-08 13:39:47 +0530

    Update sunderam.c

--- sunderam.c
+++ sunderam.c
@@ -4,7 +4,7 @@ int convertBinaryToDecimal(int n);
 int main()
 {
     int n;
-    int Val =5;
+    int Val =1;
     printf("Enter a binary number: ");
     scanf("%d", &n);
     printf("%d in binary = %d in decimal", n, convertBinaryToDecimal(n));
@@ -15,9 +15,9 @@ int convertBinaryToDecimal(int n)
     int decimalNumber = 0, i = 0, remainder;
     while (n!=0)
     {
-        remainder = n%10;
-        n = n/ 10;
-        decimalNumber = decimalNumber + remainder*pow(2,i);
+       remainder = n%10;
+        n /= 10;
+        decimalNumber += remainder*pow(2,i);
         ++i;
     }
     return decimalNumber;
